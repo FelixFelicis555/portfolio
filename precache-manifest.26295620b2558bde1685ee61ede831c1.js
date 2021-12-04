6a97eeb96db8a51ddf0299b90159ece990348ae9
FelixFelicis555 <perfectpawi13@gmail.com>
2021-12-04 22:45:26 +0530

    Updates

---
+++ precache-manifest.26295620b2558bde1685ee61ede831c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6347f0b6c81052b6efcbd767e5de08ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9e4ef9a3217a7474b40",
+    "url": "/portfolio/static/css/main.13754439.chunk.css"
+  },
+  {
+    "revision": "5870144c2ad3363ed664",
+    "url": "/portfolio/static/js/2.4c082151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c082151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e4ef9a3217a7474b40",
+    "url": "/portfolio/static/js/main.bc7b47d2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
